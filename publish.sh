a87cab8aca5634fd339271509f60f238bb4778e1
Hizbullah Abdul Aziz <hizbullah.abdul@protonmail.com>
2019-02-08 23:11:39 +0100

    Add publish script

---
+++ publish.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+rsync -a --filter='P _site/' \
+	 --filter='P _cache/' \
+	 --filter='P .git/' \
+	 --filter='P .gitignore' \
+	 --filter='P .stack-work' \
+	 --delete-excluded \
+	 _site/ .
